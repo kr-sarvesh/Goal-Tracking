de1737c980976e4dc287ab2cdf16f43e297135ed
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-02-13 00:44:29 +0530

    hashed the password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,43 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
-const User = require('../models/User')
-const asyncHandler = require('../middleware/errorHandler')
+const User = require('../models/userModel')
+const asyncHandler = require('express-async-handler')
+/*
+ * @desc    Register a new user
+ */
 exports.registerUser = asyncHandler(async (req, res) => {
-  res.json({
-    message: 'Register User',
+  const { name, email, password } = req.body
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please enter all fields')
+  }
+  // check if the user already exists
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+  // Hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  // Create a new user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
   })
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
+
 exports.loginUser = asyncHandler(async (req, res) => {
   res.json({
     message: 'Login User',
