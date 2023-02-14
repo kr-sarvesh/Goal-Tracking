e3842a0dbeb9bddf3639d59d5c094c74f763528d
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-02-14 20:44:41 +0530

    bug: decoded part added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -14,6 +14,8 @@ const protect = asyncHandler(async (req, res, next) => {
       //Get token from header
       token = req.headers.authorization.split('')[1]
       // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      // Get user from the Token
       req.user = await User.findById(decoded.id).select('-password')
       next()
     } catch (error) {
