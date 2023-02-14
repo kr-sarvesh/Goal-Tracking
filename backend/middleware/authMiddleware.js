8610996f6a797e3164c41fca8cd27717b39456d9
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-02-14 23:51:12 +0530

    update: verification of the token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,15 +12,17 @@ const protect = asyncHandler(async (req, res, next) => {
   ) {
     try {
       //Get token from header
-      token = req.headers.authorization.split('')[1]
+      token = req.headers.authorization.split(' ')[1]
+
       // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
       // Get user from the Token
       req.user = await User.findById(decoded.id).select('-password')
       next()
     } catch (error) {
       console.log(error)
-      res.status(401)
+      res.status(401).send('Invalid Token')
       throw new Error('Not Authorized')
     }
   }
