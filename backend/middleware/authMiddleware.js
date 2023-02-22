260ed5f093b132f891463ca72b35ad208316ddc3
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-02-22 11:31:04 +0530

    removed consoles

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -19,6 +19,7 @@ const protect = asyncHandler(async (req, res, next) => {
 
       // Get user from the Token
       req.user = await User.findById(decoded.id).select('-password')
+      console.log(req.user)
       next()
     } catch (error) {
       console.log(error)
