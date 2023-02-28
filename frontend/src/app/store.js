9127fcb1bf70a3f8c438c212ec31258f42bee718
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-02-28 23:49:07 +0530

    set up the configure store with authentication

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '../features/auth/authSlice'
+// The store now has redux-thunk added in and the Redux DevTools Extension is turned on
+//ReduxThunk middleware for Redux. It allows writing functions with logic inside that can interact with a Redux store's dispatch and getState methods.
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: authReducer,
   },
-});
+})
