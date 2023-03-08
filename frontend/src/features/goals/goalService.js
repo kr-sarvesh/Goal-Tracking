01961926763a97c086f69597c84c99d1392bb659
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-09 01:40:23 +0530

    added getgoals

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 const API_URL = '/api/goals'
 
 //create new goal
-const createGoal = async (goalData, token) => {
+const createGoal = async (goalData) => {
   const response = await axios.post(API_URL, goalData)
   return response.data
 }
-const goalService = { createGoal }
+const getGoals = async () => {
+  const response = await axios.get(API_URL)
+  return response.data
+}
+const goalService = { createGoal, getGoals }
 export default goalService
